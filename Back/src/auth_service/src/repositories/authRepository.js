a0810155ff55380372fc7239fb39a0ba420235b8
Agustin Azorin <azorin.agustin231106@gmail.com>
2025-12-03 20:59:03 -0300

    reset

--- Back/src/auth_service/src/repositories/authRepository.js
+++
@@ -1,7 +0,0 @@
-import Session from "../models/session.js";
-
-export default {
-  createSession: (data) => Session.create(data),
-  invalidateSession: (id) =>
-    Session.update({ is_valid: false }, { where: { id } }),
-};
