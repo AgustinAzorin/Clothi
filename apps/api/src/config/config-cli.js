c59785d1204b79a359f6941cc412017c72a618e1
Agustin Azorin <azorin.agustin231106@gmail.com>
2025-12-04 20:21:33 -0300

    seuelize

---
+++ apps/api/src/config/config-cli.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+module.exports = {
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  },
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: "postgres"
+  }
+};
